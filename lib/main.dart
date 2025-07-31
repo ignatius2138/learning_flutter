0a3b4ef41a8880eb6383fdddd83d3037e2f5910d
Vladlen Astakhov <vladlenastakhov@gmail.com>
2025-08-01 00:56:56 +0300

    Added basic Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,25 @@ class MyApp extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(title: Text("Kydja"), centerTitle: true),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.plus_one),
+        ),
+        drawer: Drawer(
+          child: Column(
+            children: [
+              DrawerHeader(child: Text("Header")),
+              ListTile(title: Text("Logout"),)
+            ],
+          ),
+        ),
         bottomNavigationBar: NavigationBar(
           destinations: [
             NavigationDestination(icon: Icon(Icons.ac_unit), label: "Home"),
-            NavigationDestination(icon: Icon(Icons.abc_sharp), label: "Second Screen"),
+            NavigationDestination(
+              icon: Icon(Icons.abc_sharp),
+              label: "Second Screen",
+            ),
           ],
         ),
       ),
