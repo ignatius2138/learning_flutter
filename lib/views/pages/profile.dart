9b162fc131138da68d19269162270f2f227bce09
Vladlen Astakhov <vladlenastakhov@gmail.com>
2025-08-06 01:13:54 +0300

    Added basic textField

--- lib/views/pages/profile.dart
+++ lib/views/pages/profile.dart
@@ -1,11 +1,35 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  TextEditingController controller = TextEditingController();
+  
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Profile Page"),
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          TextField(
+            onEditingComplete: () {
+              setState(() {
+
+              });
+            },
+            controller: controller,
+            decoration: InputDecoration(
+              border: OutlineInputBorder()
+            ),
+          ),
+          Text(controller.text)
+        ],
+      ),
     );
   }
 }
